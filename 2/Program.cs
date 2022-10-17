198c4b29e5039522158f648c286f33210982c657
Fedor <kurnosenkovf@mail.ru>
2022-10-17 22:48:03 +0400

    2

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine ("введите два числа:");
+
+var numberA = Convert.ToInt32(Console.ReadLine());
+var numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberA >  numberB)
+
+{
+    Console.WriteLine ();
+    Console.Write("max=");
+    Console.WriteLine(numberA);
+   }
+
+else
+{
+    Console.WriteLine();
+    Console.Write("max=");
+    Console.WriteLine(numberB);
+}
+
