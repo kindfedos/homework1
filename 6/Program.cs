0f6de9deeb983b2575ce2edf4ff02b2c7cc3bc7b
Fedor <kurnosenkovf@mail.ru>
2022-10-17 23:56:12 +0400

    6

---
+++ 6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine ("введите число");
+
+var numberA = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberA % 2 == 1)
+{
+  Console.WriteLine(" Число " + numberA + " нечетное ");
+}
+
+else
+{
+   Console.WriteLine(" Число " + numberA + " четное ");
+}
+
+
+  
