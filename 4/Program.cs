71ff532b07ca48ebdabaae8847213bc1fb66b321
Fedor <kurnosenkovf@mail.ru>
2022-10-17 23:15:43 +0400

    4

---
+++ 4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine ("введите три числа:");
+
+var numberA = Convert.ToInt32(Console.ReadLine());
+var numberB = Convert.ToInt32(Console.ReadLine());
+var numberC = Convert.ToInt32(Console.ReadLine());
+
+int Max = numberA;
+
+if (numberB > Max)
+{
+    Max = numberB;
+}
+if (numberC > Max) 
+{ 
+    Max = numberC;
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine (Max);
